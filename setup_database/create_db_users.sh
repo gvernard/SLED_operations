5c6420604f14611666134545402cc37373b45651
Giorgos <gvernardos@amnh.org>
2024-03-02 15:07:11 -0500

    Prepared new database initialization and reset scripts.

--- setup_database/create_db_users.sh
+++ setup_database/create_db_users.sh
@@ -1,26 +1,97 @@
 #!/bin/bash
 
-root_path=`pwd`/../.. 
-cnf_file=${root_path}/SLED_secrets/root.cnf
 
 # Detect paths
 MYSQL=$(which mysql)
 AWK=$(which awk)
 GREP=$(which grep)
+current_path=`pwd`
+secret_path=${current_path}/../../SLED_secrets
+if ! [ -d ${secret_path} ]
+then
+    echo "Directory ${secret_path} not found!"
+    exit
+fi
+
 
 # Get database connection parameters
+cnf_file=${secret_path}/root.cnf
 MHOST=$($AWK '/^host/{print $3}' $cnf_file)
 MPORT=$($AWK '/^port/{print $3}' $cnf_file)
 MDB=$($AWK '/^database/{print $3}' $cnf_file)
 MUSER=$($AWK '/^user/{print $3}' $cnf_file)
 MPASS=$($AWK '/^password/{print $3}' $cnf_file)
 
+
 # Create database
 $MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS -e "CREATE DATABASE $MDB"
+#$MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS $MDB -e "SET FOREIGN_KEY_CHECKS = 1;"
+$MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS $MDB -e "ALTER DATABASE $MDB CHARACTER SET utf8 COLLATE utf8_bin;"
+$MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS $MDB < function_distance_on_sky.sql
 
 
-# Create SLED_ROOT user
-$cnf_file=${root_path}/SLED_secrets/sled_root.cnf
-USER=$($AWK '/^user/{print $3}' $cnf_file)
-PASS=$($AWK '/^password/{print $3}' $cnf_file)
-$MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS -e "CREATE USER 'sled_root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$PASS'";
+# Create SLED_ROOT user (can create, alter, delete tables)
+$cnf_file=${secret_path}/sled_root.cnf
+if [ -f $cnf_file ]
+then
+    USER=$($AWK '/^user/{print $3}' $cnf_file)
+    PASS=$($AWK '/^password/{print $3}' $cnf_file)
+    $MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS -e "CREATE USER '$USER'@'localhost' IDENTIFIED WITH mysql_native_password BY '$PASS'";
+    $status1=$?
+    $MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, INDEX, DROP, ALTER, CREATE TEMPORARY TABLES, LOCK TABLES ON ${MDB}.* TO '$USER'@'localhost';"
+    $status3=$?
+    $MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS $MDB -e "GRANT EXECUTE ON FUNCTION ${MDB}.distance_on_sky TO '$USER'@'localhost';"
+    $status2=$?
+    if [ $status1 -eq 0 ] && [ $status2 -eq 0 ] && [ $status3 -eq 0 ]
+    then
+	echo "MySQL user $USER created successfully."
+    else
+	echo "Something went wrong whn creating $USER"
+else
+    echo "File $cnf_file not found!"    
+fi
+
+
+# Create SLED_RW user (read and write)
+$cnf_file=${secret_path}/sled_rw.cnf
+if [ -f $cnf_file ]
+then
+    USER=$($AWK '/^user/{print $3}' $cnf_file)
+    PASS=$($AWK '/^password/{print $3}' $cnf_file)
+    $MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS -e "CREATE USER '$USER'@'localhost' IDENTIFIED WITH mysql_native_password BY '$PASS'";
+    $status1=$?
+    $MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS -e "GRANT SELECT,INSERT,UPDATE,DELETE ON $MDB.* TO '$USER'@'localhost';"
+    $status2=$?
+    $MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS $MDB -e "GRANT EXECUTE ON FUNCTION ${MDB}.distance_on_sky TO '$USER'@'localhost';"
+    $status3=$?
+    if [ $status1 -eq 0 ] && [ $status2 -eq 0 ] && [ $status3 -eq 0 ]
+    then
+	echo "MySQL user $USER created successfully."
+    else
+	echo "Something went wrong whn creating $USER"
+else
+    echo "File $cnf_file not found!"    
+fi
+
+
+# Create SLED_RO user (read only)
+$cnf_file=${secret_path}/SLED_secrets/sled_ro.cnf
+if [ -f $cnf_file ]
+then
+    USER=$($AWK '/^user/{print $3}' $cnf_file)
+    PASS=$($AWK '/^password/{print $3}' $cnf_file)
+    $MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS -e "CREATE USER '$USER'@'localhost' IDENTIFIED WITH mysql_native_password BY '$PASS'";
+    $status1=$?
+    $MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS -e "GRANT SELECT ON $MDB.* TO '$USER'@'localhost';"
+    $status2=$?
+    $MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS $MDB -e "GRANT EXECUTE ON FUNCTION ${MDB}.distance_on_sky TO '$USER'@'localhost';"
+    $status3=$?
+    if [ $status1 -eq 0 ] && [ $status2 -eq 0 ] && [ $status3 -eq 0 ]
+    then
+	echo "MySQL user $USER created successfully."
+    else
+	echo "Something went wrong whn creating $USER"
+else
+    echo "File $cnf_file not found!"    
+fi
+
--- setup_database/drop_all_tables.sh
+++ setup_database/drop_all_tables.sh
@@ -1,34 +1,38 @@
 #!/bin/bash
 
-cnf_file=$1
 
 # Detect paths
 MYSQL=$(which mysql)
 AWK=$(which awk)
 GREP=$(which grep)
+current_path=`pwd`
+secret_path=${current_path}/../../SLED_secrets
+if ! [ -d ${secret_path} ]
+then
+    echo "Directory ${secret_path} not found!"
+    exit
+fi
+
 
 # Get database connection parameters
+cnf_file=${secret_path}/sled_root.cnf
+if ! [ -f $cnf_file ]
+then
+    echo "File $cnf_file not found!"    
+    exit
+fi
 MHOST=$($AWK '/^host/{print $3}' $cnf_file)
 MPORT=$($AWK '/^port/{print $3}' $cnf_file)
 MDB=$($AWK '/^database/{print $3}' $cnf_file)
 MUSER=$($AWK '/^user/{print $3}' $cnf_file)
 MPASS=$($AWK '/^password/{print $3}' $cnf_file)
-#echo $MHOST $MPORT $MDB $MUSER $MPASS
-#exit 0
 
 
+# Delete all tables
 TABLES=$($MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS $MDB -e 'show tables' | $AWK '{ print $1}' | $GREP -v '^Tables' )
-
 for t in $TABLES
 do
     echo "Deleting table: $t"
     $MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS $MDB -e "SET FOREIGN_KEY_CHECKS=0; DROP TABLE $t;" # The FOREIGN_KEY_CHECKS applies per connection.
 done
-$MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS $MDB -e "DROP FUNCTION distance_on_sky;"
 
-
-#$MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS $MDB -e "SET FOREIGN_KEY_CHECKS = 1;"
-$MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS $MDB -e "ALTER DATABASE $MDB CHARACTER SET utf8 COLLATE utf8_bin;"
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-$MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS $MDB < $SCRIPTPATH/function_distance_on_sky.sql
-$MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS $MDB -e "GRANT EXECUTE ON FUNCTION strong_lenses_database.distance_on_sky TO 'sled_rw'@'10.25.64.150';"
