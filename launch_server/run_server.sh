485c82d7ea5f6a55f192a15c9c684bd448e89232
Giorgos <gvernardos@amnh.org>
2024-03-07 11:42:42 -0500

    Create script to setup server enviroment variables and settings before interaction.

--- launch_server/run_server.sh
+++ launch_server/run_server.sh
@@ -1,54 +1,17 @@
 #!/bin/bash
 
-if [ $# != 1 ]
+if [ $# != 2 ]
 then
-    echo "One command line arguments are required: "
+    echo "Two command line arguments are required: "
     echo "  1 - the mode of the django server: 'production' or 'debug' or 'production_ro'"
+    echo "  2 - the full path to the SLED project directory, i.e. the directory containing SLED_api"
     exit 0
 fi
 mode=$1
+root_path=$2
 
 
-
-root_path=`pwd`/../..
-secret_path=`pwd`/../../SLED_secrets
-
-
-
-# Export environment variables and set the settings.py file
-export DJANGO_SLACK_API_TOKEN=`cat ${secret_path}/slack_api_token.txt`
-
-
-
-if [ $mode = "debug" ]
-then
-    export DJANGO_SECRET_KEY='django-insecure-3#$_(o_0g=w68gw@y5anq4$yb2$b!&1_@+bk%jse$*mboql#!t'
-    export DJANGO_EMAIL_PASSWORD=`cat ${secret_path}/email_password.txt`
-    export DJANGO_MEDIA_ROOT=/debug/FILES
-    export DJANGO_STATIC_ROOT=/debug/STATIC
-    export DJANGO_DB_FILE=${secret_path}/sled_rw.cnf
-    cp settings_debug.py ${root_path}/SLED_api/mysite/settings.py
-else
-    export DJANGO_SECRET_KEY=`cat ${secret_path}/secret_key.txt`
-    export DJANGO_EMAIL_PASSWORD=`cat ${secret_path}/email_password.txt`
-    export DJANGO_MEDIA_ROOT=/production/FILES
-    export DJANGO_STATIC_ROOT=/production/STATIC
-    if [ $mode = "production" ]
-    then
-	export DJANGO_DB_FILE=`pwd`/sled_rw.cnf
-	export DJANGO_NO_LAST_LOGIN=false	
-    elif [ $mode = "production_ro" ]
-    then
-	export DJANGO_DB_FILE=`pwd`/sled_ro.cnf
-	export DJANGO_NO_LAST_LOGIN=true
-    else
-	echo "Unknown mode (1st argument)! It must be 'production', 'production_ro', or 'debug'"
-	exit 0
-    fi
-    cp settings_production.py ${root_path}/SLED_api/mysite/settings.py
-fi
-
-
+sudo ${root_path}/SLED_operations/launch/set_server.sh $mode $root_path
 cd ${root_path}/SLED_api
 sudo -E python3 manage.py collectstatic --noinput
 sudo -E python3 manage.py runserver 216.73.242.43
