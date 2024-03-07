2ff4fefef4bc6c4463edd57abfde0a25b686133d
Giorgos <gvernardos@amnh.org>
2024-03-07 11:55:44 -0500

    Using source instead of just running the script.

--- launch_server/run_server.sh
+++ launch_server/run_server.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
-if [ $# != 2 ]
-then
-    echo "Two command line arguments are required: "
-    echo "  1 - the mode of the django server: 'production' or 'debug' or 'production_ro'"
-    echo "  2 - the full path to the SLED project directory, i.e. the directory containing SLED_api"
-    exit 0
-fi
-mode=$1
-root_path=$2
+# Sets mode, root_path, environment variables and copies the correct setting.py file
+source ${root_path}/SLED_operations/launch/set_server.sh $1 $2
 
-
-sudo ${root_path}/SLED_operations/launch/set_server.sh $mode $root_path
+# Execute server
 cd ${root_path}/SLED_api
 sudo -E python3 manage.py collectstatic --noinput
 sudo -E python3 manage.py runserver 216.73.242.43:8000
-
-
