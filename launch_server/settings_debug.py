9d90d58d8b25e15851a826d297b33df25d8e2be5
Giorgos <gvernardos@amnh.org>
2024-03-07 20:34:26 -0500

    Still trying to define the s3 static url.

--- launch_server/settings_debug.py
+++ launch_server/settings_debug.py
@@ -19,21 +19,25 @@ DATABASES = {
 }
 
 
+S3=False
+if S3:
+    AWS_ACCESS_KEY_ID = os.environ['S3_ACCESS_KEY_ID']
+    AWS_SECRET_ACCESS_KEY = os.environ['S3_SECRET_ACCESS_KEY']
+    AWS_STORAGE_BUCKET_NAME = os.environ['S3_STORAGE_BUCKET_NAME']
+    AWS_S3_ENDPOINT_URL = os.environ['S3_ENDPOINT_URL']
+    AWS_DEFAULT_ACL = 'public-read'
+    #AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    #AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.{AWS_S3_ENDPOINT_URL}'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # s3 static settings
+    AWS_LOCATION = 'static'
+    #STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+    STATIC_URL = f'{AWS_S3_ENDPOINT_URL}/{AWS_LOCATION}/'
+    STATICFILES_STORAGE = 'storages.backends.s3.S3Storage'
+else:
+    STATIC_URL = 'static/'
+    STATIC_ROOT = os.environ['DJANGO_STATIC_ROOT']
 
-AWS_ACCESS_KEY_ID = os.environ['S3_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['S3_SECRET_ACCESS_KEY']
-AWS_STORAGE_BUCKET_NAME = os.environ['S3_STORAGE_BUCKET_NAME']
-AWS_S3_ENDPOINT_URL = os.environ['S3_ENDPOINT_URL']
-AWS_DEFAULT_ACL = 'public-read'
-#AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.{AWS_S3_ENDPOINT_URL}'
-AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-# s3 static settings
-AWS_LOCATION = 'static'
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-STATICFILES_STORAGE = 'storages.backends.s3.S3Storage'
 
-
+    
 MEDIA_ROOT = os.environ['DJANGO_MEDIA_ROOT']
-#STATIC_URL = 'static/'
-#STATIC_ROOT = os.environ['DJANGO_STATIC_ROOT']
