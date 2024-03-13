9fd518a9b92514a263c17ca7122bf9de88355592
Giorgos <gvernardos@amnh.org>
2024-03-13 12:50:12 -0400

    Making a backup.

--- cronjobs/daily_backup.sh
+++ cronjobs/daily_backup.sh
@@ -1,27 +1,75 @@
 #!/bin/bash
 # This script is run daily but AFTER the monthly backup script
-
-readonly BACKUP_DIR=$1"/FILES"
-readonly BACKUP_DATABASE_DIR=$1"/database"
-readonly SOURCE_DIR=$2"/FILES"
-cnf_file=$2"/launch_server/server_production.cnf"
 MYSQLDUMP=$(which mysqldump)
 AWK=$(which awk)
+current_path=`pwd`
+secret_path=${current_path}/../../SLED_secrets
 
-readonly TARGET="$(date '+%0d')"
+
+
+########## SETUP TARGET DIRECTORY ##########
+if [[ $# -eq 1 ]]
+then
+    readonly NAME=$1
+else
+    readonly NAME="latest"
+fi
+readonly BACKUP_DIR="/var/tmp/SLED_tmp_backups/"${NAME}
+if [ -d $BACKUP_DIR ]; then
+    echo "A directory with the name - $NAME - already exists."
+    while true; do
+	read -p " Proceeding will overwrite it. Are you sure you want to proceed (y/n)? " yn
+	case $yn in
+            [Yy]* ) mkdir -p ${BACKUP_DIR}; mkdir -p ${BACKUP_DIR}/database; mkdir -p ${BACKUP_DIR}/files; break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes (y) or no (n).";;
+	esac
+    done
+    
+fi
 
 
 
 ########## BACKUP DATABASE ##########
+
 # Get database connection parameters
+cnf_file=${secret_path}/sled_ro.cnf
+if ! [ -f $cnf_file ]
+then
+    echo "File $cnf_file not found!"    
+    exit
+fi
 MHOST=$($AWK '/^host/{print $3}' $cnf_file)
 MPORT=$($AWK '/^port/{print $3}' $cnf_file)
 MDB=$($AWK '/^database/{print $3}' $cnf_file)
 MUSER=$($AWK '/^user/{print $3}' $cnf_file)
 MPASS=$($AWK '/^password/{print $3}' $cnf_file)
 
-# Full database backups using mysqldump
-mysqldump -h $MHOST -P $MPORT -u $MUSER –p$MPASS $MDB > ${BACKUP_DATABASE_DIR}/${TARGET}.sql
+# Full database backup using mysqldump
+mysqldump -h $MHOST -P $MPORT -u $MUSER –p$MPASS $MDB > ${BACKUP_DIR}/database/strong_lenses_database.sql
+
+
+
+
+
+########## TRANSFER TO REMOTE STORAGE ##########
+
+# Transfer backup file to remote backup storage
+conf_file=${secret_path}/rclone.conf
+if ! [ -f $conf_file ]
+then
+    echo "File $conf_file not found!"    
+    exit
+fi
+rclone --config="${conf_file}" sync ${BACKUP_DIR} sled_backup:${NAME}
+rm -r ${BACKUP_DIR}
+rclone copy sled_storage:static sled_backup:${NAME}/files
+
+
+
+
+
+exit
 
 
 ########## BACKUP FILES ##########
