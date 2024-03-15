531eb70a99f92eb156742300dd4544f3d422dda2
Giorgos <gvernardos@amnh.org>
2024-03-15 12:05:19 -0400

    Empty storage does not remove static files.

--- restore_backup/empty_storage.sh
+++ restore_backup/empty_storage.sh
@@ -22,7 +22,7 @@ echo "DANGER!!! This will wipe-out the production storage/static files."
 while true; do
     read -p " Are you DEFINITELY sure you want to proceed (y/n)? " yn
     case $yn in
-        [Yy]* ) rclone --config="${conf_file}" delete sled_storage:/stronglens01 --rmdirs; break;;
+        [Yy]* ) rclone --config="${conf_file}" delete sled_storage:/stronglens01/files --rmdirs; break;;
         [Nn]* ) exit 1;;
         * ) echo "Please answer yes (y) or no (n).";;
     esac
