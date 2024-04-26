c1d4332326971a85eb6967f8a8e19c0acc4b481e
Giorgos <gvernardos@amnh.org>
2024-04-25 20:08:32 -0400

    Restored settings for debug and production.

--- launch_server/settings_production.py
+++ launch_server/settings_production.py
@@ -3,11 +3,21 @@ from mysite.settings_common import *
 
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
-DEBUG = True
+DEBUG = False
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = ['127.0.0.1','127.0.1.1','localhost','sled.amnh.org','216.73.242.43']
+
+EMAIL_HOST_PASSWORD = os.environ['DJANGO_EMAIL_PASSWORD']
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'sleddatabase@gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 
 DATABASES = {
     'default': {
@@ -18,18 +28,14 @@ DATABASES = {
     }
 }
 
+if os.environ['DJANGO_NO_LAST_LOGIN'] == 'true':
+    NO_UPDATE_LAST_LOGIN = True
+else:
+    NO_UPDATE_LAST_LOGIN = False
 
-AWS_ACCESS_KEY_ID = os.environ['S3_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['S3_SECRET_ACCESS_KEY']
-AWS_STORAGE_BUCKET_NAME = os.environ['S3_STORAGE_BUCKET_NAME']
-AWS_S3_ENDPOINT_URL = os.environ['S3_ENDPOINT_URL']
-AWS_DEFAULT_ACL = None
-AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 
-STATIC_LOCATION = 'static'
-STATIC_URL = f'{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/{STATIC_LOCATION}/'
-STATICFILES_STORAGE = 'mysite.storage_backends.StaticStorage'
-
-DATABASE_FILE_LOCATION = 'files'
-MEDIA_URL = f'{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/{DATABASE_FILE_LOCATION}/'
-DEFAULT_FILE_STORAGE = 'mysite.storage_backends.DatabaseFileStorage'
+# Specific for the server
+FORCE_SCRIPT_NAME = '/'
+STATIC_URL = 'staticfiles/'
+MEDIA_ROOT = os.environ['DJANGO_MEDIA_ROOT']
+STATIC_ROOT = os.environ['DJANGO_STATIC_ROOT']
