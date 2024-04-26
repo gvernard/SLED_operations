8a74ff67442c9c9e0b5d965c119fae483f0c5824
Giorgos <gvernardos@amnh.org>
2024-04-25 20:47:39 -0400

    Modified further the run_server script.

--- launch_server/run_server.sh
+++ launch_server/run_server.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-if [ $# != 1 ]
-then
-    echo "One command line arguments are required: "
-    echo "  1 - the full path to the SLED project directory, i.e. the directory containing SLED_api"
-    exit 0
-fi
-
-root_path=${1%/}
+secret_path=/home/gvernardos/SLED/SLED_secrets
+site_path=/home/gvernardos/SLED
 mypython=/usr/local/bin/python3.12
 
-# Sets environment variables and copies the correct setting.py file
-mode=debug
-source ${root_path}/SLED_operations/launch_server/set_server.sh
+bash create_env.sh ${secret_path} migrate
+source sled-envvars.env
 
-# Execute server
-cd ${root_path}/SLED_api
+cd ${site_path}/SLED_api
 sudo -E ${mypython} manage.py runserver 216.73.242.43:8000
