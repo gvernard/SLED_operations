57786d8076bf4449503f748db4db5477916f726e
Giorgos <gvernardos@amnh.org>
2024-05-03 22:23:48 -0400

    Fixed debug and production settings.py.

--- launch_server/settings_production.py
+++ launch_server/settings_production.py
@@ -33,9 +33,17 @@ if os.environ['DJANGO_NO_LAST_LOGIN'] == 'true':
 else:
     NO_UPDATE_LAST_LOGIN = False
 
+AWS_ACCESS_KEY_ID = os.environ['S3_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['S3_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['S3_STORAGE_BUCKET_NAME']
+AWS_S3_ENDPOINT_URL = os.environ['S3_ENDPOINT_URL']
+AWS_DEFAULT_ACL = None
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 
-# Specific for the server
-FORCE_SCRIPT_NAME = '/'
-STATIC_URL = 'staticfiles/'
-MEDIA_ROOT = os.environ['DJANGO_MEDIA_ROOT']
-STATIC_ROOT = os.environ['DJANGO_STATIC_ROOT']
+STATIC_LOCATION = 'static'
+STATIC_URL = f'{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/{STATIC_LOCATION}/'
+STATICFILES_STORAGE = 'mysite.storage_backends.StaticStorage'
+
+DATABASE_FILE_LOCATION = 'files'
+MEDIA_URL = f'{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/{DATABASE_FILE_LOCATION}/'
+DEFAULT_FILE_STORAGE = 'mysite.storage_backends.DatabaseFileStorage'
